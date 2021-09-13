7cfaab5c9f64468982a31bfc5a4889ad2dad1853
sohan-fahad <sohanfahad@gmail.com>
2021-09-13 14:54:27 +0600

    fixed error

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+// This function will load data when page is running
 const loadProducts = () => {
   const url = `https://fakestoreapi.com/products`;
   fetch(url)
@@ -6,52 +8,91 @@ const loadProducts = () => {
 };
 loadProducts();
 
-// show all product in UI 
+//This function will show all product in UI 
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
-    const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
+    const image = product.image;
+    const { rate, count } = product.rating
+    const productDisplay = document.createElement("div");
+    productDisplay.classList.add("product");
+    productDisplay.innerHTML = `<div class="single-product">
+      <div class= "img-section">
+    <img class="product-image img-fluid" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h4>${product.title.slice(0, 25)}</h4>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <h4><span class="text-danger">Rating: ${rate}</span> || <span class="text-primary">Reviews: ${count}</span?</h4>
+      <h4 class="text-danger">Price: $ ${product.price}</h4>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="add-cart">add to cart</button>
+      <button onclick="itemDetails(${product.id})" class="detail-btn">Details</button></div>
       `;
-    document.getElementById("all-products").appendChild(div);
+    document.getElementById("all-products").appendChild(productDisplay);
   }
 };
+
+// this function load single item details
+const itemDetails = id => {
+  const url = `https://fakestoreapi.com/products/${id}`
+  fetch(url)
+    .then(res => res.json())
+    .then(data => showDetails(data))
+}
+
+// this  funcion will show details of an item
+const showDetails = item => {
+  document.getElementById('detailsItem').innerHTML = `
+      <div class="card detailsCard">
+      <div class="col-md-4 img-section">
+        <img src="${item.image}" class="img-fluid product-image rounded-start" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h3 class="card-title">${item.title}</h3>
+          <h4 class="text-danger">Price: $ ${item.price}</h4>
+          <p class="card-text">${item.description}</p>
+          <button class="detail-btn" onclick="closeDetails()">Close</button></div>
+        </div>
+      </div>
+    </div>
+  `
+}
+
+// this function will close details from top
+const closeDetails = () =>{
+  document.getElementById('detailsItem').textContent = ''
+}
+
 let count = 0;
+
+// this function will calculate & update cart data 
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
 
   updateTaxAndCharge();
+  updateTotal()
   document.getElementById("total-Products").innerText = count;
 };
 
+// this function will get the dom by id
 const getInputValue = (id) => {
-  const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
-  return converted;
+  const elementName = document.getElementById(id).innerText;
+  const convertedElemen = parseFloat(elementName);
+  return convertedElemen;
 };
 
-// main price update function
+// this function will update the main price
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
-// set innerText function
+// this funnction will get the DOM set innerText by id
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -71,10 +112,10 @@ const updateTaxAndCharge = () => {
   }
 };
 
-//grandTotal update function
+// this function will calculate grandTotal
 const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
